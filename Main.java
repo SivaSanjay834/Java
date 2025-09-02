5126f266396196c229dbe729a299b2e17d8f55aa
SivaSanjay834 <uppalapatisivasanjay@gmail.com>
2025-09-02 09:39:51 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ class Main{
 public static void main(String args[]){
 System.out.println("Hello welcome to the GitHub");
 System.out.println("Welcome to Jennkins");
+  System.out.println("Hello Everyone");
 }
+
 }

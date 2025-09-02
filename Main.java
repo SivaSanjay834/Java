e8f581f0e3f120387c54619a949effef96df3608
SivaSanjay834 <uppalapatisivasanjay@gmail.com>
2025-09-02 09:48:10 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,8 @@ public static void main(String args[]){
 System.out.println("Hello welcome to the GitHub");
 System.out.println("Welcome to Jennkins");
   System.out.println("Hello Everyone");
+    System.out.println("Welcome to devops lab");
 }
 
 }
+
